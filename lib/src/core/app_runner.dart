554259c2cacf4a9e76361936a6bc0875221bd137
Hawmex <hamed.araab.02@gmail.com>
2023-08-02 19:33:56 +0330

    feat: add more core files

---
+++ lib/src/core/app_runner.dart
@@ -0,0 +1,30 @@
+import 'package:js/js.dart';
+
+import '../widgets/widget.dart';
+
+@JS('__navandAppNode__')
+external Node? _appNode;
+
+/// Attaches the given [app] to the document's body by initializing it.
+void runApp(final Widget app) {
+  // ignore: avoid_print
+  print(r'''
+  _   _                            _ 
+ | \ | |                          | |
+ |  \| | __ ___   ____ _ _ __   __| |
+ | . ` |/ _` \ \ / / _` | '_ \ / _` |
+ | |\  | (_| |\ V / (_| | | | | (_| |
+ |_| \_|\__,_| \_/ \__,_|_| |_|\__,_|
+                                                       
+''');
+
+  if (_appNode != null && app.matches(_appNode!.widget)) {
+    _appNode!.widget = app;
+  } else {
+    _appNode?.dispose();
+
+    _appNode = app.createNode()
+      ..parentNode = null
+      ..initialize();
+  }
+}
---
+++ lib/src/core/async_widget_builder.dart
@@ -0,0 +1,12 @@
+import '../widgets/future_builder.dart';
+import '../widgets/stream_builder.dart';
+import '../widgets/widget.dart';
+import 'async_snapshot.dart';
+import 'build_context.dart';
+
+/// The type of the builder function used in async builders such as
+/// [StreamBuilder] and [FutureBuilder].
+typedef AsyncWidgetBuilder<T> = Widget Function(
+  BuildContext context,
+  AsyncSnapshot<T> snapshot,
+);
