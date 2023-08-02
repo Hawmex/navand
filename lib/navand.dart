d64413e8119a1bbe532df7e8d56510c9dda67f85
Hawmex <hamed.araab.02@gmail.com>
2023-08-02 12:57:42 +0330

    docs: update library docs

--- lib/navand.dart
+++ lib/navand.dart
@@ -1,4 +1,4 @@
-/// Navand is a Dart web framework that lets developers create UIs with a widget
-/// model similar to Flutter. Navand apps are compiled into JS and painted with
-/// HTML & CSS.
+/// Navand, meaning "a swift horse" in Persian, is a Dart web framework that
+/// lets developers create UIs with a widget model similar to Flutter. Navand
+/// apps are compiled into JS and painted with HTML & CSS.
 library;
